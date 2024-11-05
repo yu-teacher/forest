18e8fc14d7156c158da2c2a2afa2f13c31c0409a
yu-teacher <119153725+yu-teacher@users.noreply.github.com>
2024-11-05 16:08:14 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+    // GitHub Pages를 위한 base path 설정
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/forest' : '',
+      base: process.env.NODE_ENV === 'production' ? '/forest' : ''
     }
-  },
-  preprocess: vitePreprocess()
+	}
 };
 
 export default config;
